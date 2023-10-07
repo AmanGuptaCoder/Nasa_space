e2897d70d26bf17b1d17677cba1081295456bf93
Aman Gupta <amanuniquecoder@gmail.com>
2023-10-07 16:00:06 +0530

    added minimal frontend+backend

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'root';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = 'root';
+$config['db']['pre'] = '';
+
+$config['admin_folder'] = 'admin';
+$config['version'] = '2.4';
+$config['installed'] = '1';
+?>
